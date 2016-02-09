adb137a21055871bef8cb97a414ce71e027e42de
Stefan Huber <stefan.huber@beyondit.at>
2016-02-09 10:25:59 +0100

    updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,20 +7,10 @@ var mocha = require('mocha');
 var KarmaServer = require('karma').Server;
 
 gulp.task('lint', function () {
-	// ESLint ignores files with "node_modules" paths.
-	// So, it's best to have gulp ignore the directory as well.
-	// Also, Be sure to return the stream from the task;
-	// Otherwise, the task may end before the stream has finished.
 	return gulp.src(['src/**/*.js','test/**/*.js','!node_modules/**'])
-			// eslint() attaches the lint output to the "eslint" property
-			// of the file object so it can be used by other modules.
-			.pipe(esl())
-			// eslint.format() outputs the lint results to the console.
-			// Alternatively use eslint.formatEach() (see Docs).
-			.pipe(esl.format())
-			// To have the process exit with an error code (1) on
-			// lint error, return the stream and pipe to failAfterError last.
-			.pipe(esl.failAfterError());
+		.pipe(esl())
+		.pipe(esl.format())
+		.pipe(esl.failAfterError());
 });
 
 gulp.task('default',['lint'],function() {
